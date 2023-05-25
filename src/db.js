dc691246e042bfbb57cb5125f4e8bded046b69f6
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-05-25 13:47:09 -0300

    cambiando url de DB para deploy

--- src/db.js
+++ src/db.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 
 //Modelos
 const {userModel, countryModel, activityModel} = require('./models/index');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
